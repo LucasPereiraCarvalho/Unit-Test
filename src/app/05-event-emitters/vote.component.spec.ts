67111e484f0d681f44fb4188299bcf2124bb8725
Lucas Pereira <lucas.carvalho@copel.com>
2022-07-06 21:44:45 -0300

    TU Event-Emitters

--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,20 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+  var component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanges event when upVoted', () => {
+    let totalVotes = 0;
+    component.voteChanged.subscribe((tv) => {
+      totalVotes = tv;
+    });
+
+    component.upVote();
+
+    expect(totalVotes).toBe(1);
   });
 });
